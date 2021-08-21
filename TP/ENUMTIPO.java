feacc78a7a0ef19602ebcfa3c65bac0c353d2b4e
Gabriel Morales <48184264+GabrielMorales1999@users.noreply.github.com>
2021-08-21 11:38:02 -0300

    Primeras clases creadas.
    Primeras clases, es simplemente para compartir los primeros .java, el proyecto aun no se crea por problemas técnicos.

---
+++ TP/ENUMTIPO.java
@@ -0,0 +1,13 @@
+package TP;
+
+public enum ENUMTIPO {
+    ADVENTURA("Adventura"),
+    PAISAJE("Paisaje"),
+    DEGUSTACION("Degustacion");
+
+    private final String tipo;
+
+    ENUMTIPO(String tipo) {
+        this.tipo = tipo;
+    }
+}
